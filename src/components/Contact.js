49ae195ee56124d03a6fab2c4c13f857e3d839af
Jande Royalty <janderoyalty@gmail.com>
2024-03-12 07:22:38 -0500

    Refactors everything

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./Contact.css";
 
 import JandeFro from "../images/Jande Royalty afro photo with pink background.png";
+import { AiOutlineMail, AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 
-import { Row, Col, Form, Button, Image } from "react-bootstrap";
+import { Row, Col, Image } from "react-bootstrap";
 
 const Contact = () => {
   return (
     <div className="content" id="my-contact">
       <Row id="my-contact--container">
+        {/* LEFT SIDE */}
         <Col id="my-contact--left" sm={12} md={12} lg={6}>
           <Row id="my-contact--dark-red-box">
             <Image
@@ -19,36 +21,45 @@ const Contact = () => {
             />
           </Row>
         </Col>
+
+        {/* RIGHT SIDE */}
         <Col id="my-contact--right" sm={12} md={12} lg={6}>
           <Row id="my-contact--header-text">Contact Me</Row>
-          <Row id="my-contact--header-text--form">
-            <Form>
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlInput1"
-              >
-                <Form.Label className="my-contact--form-header">
-                  Email address
-                </Form.Label>
-                <Form.Control type="email" placeholder="name@example.com" />
-              </Form.Group>
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlTextarea1"
-              >
-                <Form.Label className="my-contact--form-header">
-                  Message
-                </Form.Label>
-                <Form.Control as="textarea" rows={8} />
-              </Form.Group>
-              <Button
-                variant="primary"
-                type="submit"
-                id="my-contact--form-button"
-              >
-                Submit
-              </Button>
-            </Form>
+          <Row className="my-contact--info">
+            <div className="my-contact--info--detail">
+              <AiOutlineMail size="50px" />
+              <Col className="my-contact--text ">
+                <a href="mailto: janderoyalty@gmail.com" className="links">
+                  janderoyalty@gmail.com
+                </a>
+              </Col>
+            </div>
+            <div className="my-contact--info--detail">
+              <AiFillLinkedin size="50px" />
+              <Col className="my-contact--text ">
+                <a
+                  className="links"
+                  href="https://www.linkedin.com/in/janderoyalty/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  linkedin.com/in/janderoyalty
+                </a>
+              </Col>
+            </div>
+            <div className="my-contact--info--detail">
+              <AiFillGithub size="50px" />
+              <Col className="my-contact--text ">
+                <a
+                  className="links"
+                  href="https://github.com/janderoyalty"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  github.com/janderoyalty
+                </a>
+              </Col>
+            </div>
           </Row>
         </Col>
       </Row>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,48 +1,13 @@
 import React from "react";
 import "./Footer.css";
 
-import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
-
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-
 const Footer = () => {
+  let getYear = () => {
+    return new Date().getFullYear();
+  };
   return (
     <div className="content" id="my-footer">
-      <Row>
-        <Col className="footer-info">
-          <a href="mailto: janderoyalty@gmail.com" className="links">
-            janderoyalty@gmail.com
-          </a>
-        </Col>
-        <Col>
-          <Row id="social-icons-container">
-            <Col className="social-icons">
-              <a
-                href="https://www.linkedin.com/in/janderoyalty/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <AiFillLinkedin size="35px" color="#6D213C"></AiFillLinkedin>
-              </a>
-            </Col>
-            <Col className="social-icons">
-              <a
-                href="https://github.com/janderoyalty"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <AiFillGithub size="35px" color="#6D213C"></AiFillGithub>
-              </a>
-            </Col>
-          </Row>
-        </Col>
-        <Col className="footer-info">
-          <a href="tel:6155385787" className="links">
-            615.538.5787
-          </a>
-        </Col>
-      </Row>
+      © {getYear()} All rights reserved - Website by Jande Royalty
     </div>
   );
 };
