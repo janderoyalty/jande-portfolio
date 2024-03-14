f9848107bb2f717cfce036481dd28fc0598cbaa0
Jande Royalty <janderoyalty@gmail.com>
2024-03-14 02:41:17 -0500

    Sets new pink and yellow waves, creates a Waves component to use

--- src/App.js
+++ src/App.js
@@ -6,58 +6,41 @@ import Skills from "./components/Skills";
 import Project from "./components/Project";
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
+import Wave from "./components/Wave";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Landing from "./components/Landing";
 
 function App() {
-  return (
-    <div className="App">
-      <div id="bg"></div>
-      <div id="app-content">
-        <Navigation></Navigation>
-        <div id="non-nav-content">
-          <Landing></Landing>
+	return (
+		<div className="App">
+			<div id="bg"></div>
+			<div id="app-content">
+				<Navigation></Navigation>
+				<div id="non-nav-content">
+					<Landing></Landing>
 
-          {/* PINK WAVE TOP */}
-          <div class="custom-shape-divider-bottom-1710142400">
-            <svg
-              data-name="Layer 1"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 1200 120"
-              preserveAspectRatio="none"
-            >
-              <path
-                d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
-                class="shape-fill"
-              ></path>
-            </svg>
-          </div>
+					{/* PINK WAVE TOP */}
+					<Wave
+						shapeCase="custom-shape-divider-bottom-1710401380"
+						shapeFill="shape-fill-pink" // PINK WAVE
+					></Wave>
 
-          <Project></Project>
+					<Project></Project>
 
-          <Skills></Skills>
-          {/* YELLOW WAVE TOP */}
-          <div class="custom-shape-divider-bottom-1710142400-B">
-            <svg
-              data-name="Layer 1"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 1200 120"
-              preserveAspectRatio="none"
-            >
-              <path
-                d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
-                class="shape-fill-B"
-              ></path>
-            </svg>
-          </div>
+					<Skills></Skills>
+					{/* YELLOW WAVE TOP */}
+					<Wave
+						shapeCase="custom-shape-divider-bottom-1710401643"
+						shapeFill="shape-fill-yellow" // YELLOW WAVE
+					></Wave>
 
-          <Contact></Contact>
-          <Footer></Footer>
-        </div>
-      </div>
-    </div>
-  );
+					<Contact></Contact>
+					<Footer></Footer>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
