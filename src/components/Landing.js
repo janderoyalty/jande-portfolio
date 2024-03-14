d4b0b4eb26143cdb1b89bf73330292d483f04aa3
Jande Royalty <janderoyalty@gmail.com>
2024-03-14 03:38:13 -0500

    Makes Landing and Projects slant more dynamic, updates Landing Navigation link from home to landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,30 +6,40 @@ import hawaii from "../images/Jande in Hawaii.png";
 import { Row, Col, Image } from "react-bootstrap";
 
 const Landing = () => {
-  return (
-    <Row className="content" id="home">
-      <Col id="landing--left" sm={12} md={12} lg={6}>
-        <Row id="landing--left--top">
-          <h1 id="landing--left--top--name">Hi! My name is Jande.</h1>
-          <div id="landing--left--top--pronounciation">
-            <div>It sounds like “Andy” with a “J”,</div>
-            <div>but it’s not spelled that way.</div>
-          </div>
-        </Row>
-        <Row id="landing--left--bottom">
-          A life-long learner & software engineer!
-        </Row>
-      </Col>
-      <Col id="landing--right" sm={12} md={12} lg={6}>
-        <Image
-          src={hawaii}
-          alt="Jande Royalty in Hawaii after a hike"
-          rounded
-          id="landing--jande-image"
-        ></Image>
-      </Col>
-    </Row>
-  );
+	return (
+		<Row className="content" id="landing">
+			<Col
+				id="landing--left"
+				sm={{ span: 12, order: 2 }}
+				md={{ span: 12, order: 2 }}
+				lg={{ span: 6, order: 1 }}
+			>
+				<Row id="landing--left--top">
+					<h1 id="landing--left--top--name">Hi! My name is Jande.</h1>
+					<div id="landing--left--top--pronounciation">
+						<div>It sounds like “Andy” with a “J”,</div>
+						<div>but it’s not spelled that way.</div>
+					</div>
+				</Row>
+				<Row id="landing--left--bottom">
+					A life-long learner & software engineer!
+				</Row>
+			</Col>
+			<Col
+				id="landing--right"
+				sm={{ span: 12, order: 1 }}
+				md={{ span: 12, order: 1 }}
+				lg={{ span: 6, order: 2 }}
+			>
+				<Image
+					src={hawaii}
+					alt="Jande Royalty in Hawaii after a hike"
+					rounded
+					id="landing--jande-image"
+				></Image>
+			</Col>
+		</Row>
+	);
 };
 
 export default Landing;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,65 +7,59 @@ import { FaExternalLinkAlt } from "react-icons/fa";
 import logo from "../images/Jande_Royalty_logo.png";
 
 const Navigation = () => {
-  return (
-    <>
-      <Navbar
-        // id={navTransparency}
-        id="navi-bar"
-        sticky="top"
-        expand="lg"
-        collapseOnSelect="true"
-        role="navigation"
-      >
-        <Container fluid>
-          <Navbar.Brand href="#home">
-            <div id="nav-img-container">
-              <img
-                id="nav-img"
-                src={logo} // Logo overlay color #6d213c
-                alt="Jande Royalty logo"
-              />
-            </div>
-          </Navbar.Brand>
-          {/* <Navbar.Brand href="#home">
-            <img
-              id="nav-img"
-              src={logo} // Logo overlay color #6d213c
-              alt="Jande Royalty logo"
-            />
-          </Navbar.Brand> */}
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav className="ms-auto my-2 my-lg-0" navbarScroll>
-              <Nav.Link className="nav-item" id="nav-words" href="#my-works">
-                My Projects
-              </Nav.Link>
-              <Nav.Link className="nav-item" id="nav-words" href="#my-skills">
-                My Skills
-              </Nav.Link>
+	return (
+		<>
+			<Navbar
+				// id={navTransparency}
+				id="navi-bar"
+				sticky="top"
+				expand="lg"
+				collapseOnSelect="true"
+				role="navigation"
+			>
+				<Container fluid>
+					<Navbar.Brand href="#landing">
+						<div id="nav-img-container">
+							<img
+								id="nav-img"
+								src={logo} // Logo overlay color #6d213c
+								alt="Jande Royalty logo"
+							/>
+						</div>
+					</Navbar.Brand>
 
-              <Nav.Link
-                className="nav-item"
-                id="nav-contacts"
-                href="#my-contact"
-              >
-                My Contact
-              </Nav.Link>
-              <Nav.Link
-                className="nav-item"
-                id="nav-resume"
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://acrobat.adobe.com/id/urn:aaid:sc:us:8e7b3f69-a3be-4717-9ffd-18cbec2abfd9"
-              >
-                My Resume <FaExternalLinkAlt size=".5em" />
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
-  );
+					<Navbar.Toggle aria-controls="navbarScroll" />
+					<Navbar.Collapse id="navbarScroll">
+						<Nav className="ms-auto my-2 my-lg-0" navbarScroll>
+							<Nav.Link className="nav-item" id="nav-words" href="#my-works">
+								My Projects
+							</Nav.Link>
+							<Nav.Link className="nav-item" id="nav-words" href="#my-skills">
+								My Skills
+							</Nav.Link>
+
+							<Nav.Link
+								className="nav-item"
+								id="nav-contacts"
+								href="#my-contact"
+							>
+								My Contact
+							</Nav.Link>
+							<Nav.Link
+								className="nav-item"
+								id="nav-resume"
+								target="_blank"
+								rel="noopener noreferrer"
+								href="https://acrobat.adobe.com/id/urn:aaid:sc:us:8e7b3f69-a3be-4717-9ffd-18cbec2abfd9"
+							>
+								My Resume <FaExternalLinkAlt size=".5em" />
+							</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</>
+	);
 };
 
 export default Navigation;
