650b3f9ad2a6593668c8d185fe2c74a574335a11
Jande Royalty <janderoyalty@gmail.com>
2024-03-15 01:59:51 -0500

    Adds small screen size, less than 550px, warning modal.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import Navigation from "./components/Navigation";
@@ -10,8 +10,32 @@ import Wave from "./components/Wave";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Landing from "./components/Landing";
+import { Modal } from "react-bootstrap";
 
 function App() {
+	const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+	const [showModal, setShowModal] = useState(false);
+
+	useEffect(() => {
+		const handleResize = () => {
+			setScreenWidth(window.innerWidth);
+		};
+
+		window.addEventListener("resize", handleResize);
+
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
+	useEffect(() => {
+		if (screenWidth < 550) {
+			setShowModal(true);
+		} else {
+			setShowModal(false);
+		}
+	}, [screenWidth]);
+
 	return (
 		<div className="App">
 			<div id="bg"></div>
@@ -38,6 +62,23 @@ function App() {
 					<Contact></Contact>
 					<Footer></Footer>
 				</div>
+			</div>
+			<div id="modal-parent">
+				<Modal
+					show={showModal}
+					contentLabel="Screen Size Message"
+					onHide={() => setShowModal(false)}
+				>
+					<div className="modal-content">
+						<h2>
+							Please view this app on a larger screen for a better experience.
+							<br />
+							<br />
+							For best experience please view on an 13" screen or larger.
+						</h2>
+						<button onClick={() => setShowModal(false)}>Close</button>
+					</div>
+				</Modal>
 			</div>
 		</div>
 	);
